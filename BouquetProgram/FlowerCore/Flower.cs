a4fd48cc6ee158570f590e7735a6671fcb31b9a4
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-06 00:07:09 +0300

    Add flower base class

---
+++ BouquetProgram/FlowerCore/Flower.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerCore
+{
+    public abstract class Flower
+    {
+        public String Name { get; protected set; }
+        public Colors Color { get; protected set; }
+        public abstract double Price { get; protected set; }
+
+        public Flower(String name, Colors color, double price)
+        {
+            Name = name;
+            Color = color;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Flower name: {Name}\nColor: {Color}\nPrice:{Price}\n";
+        }
+    }
+    public enum Colors
+    {
+        Red,
+        Green,
+        Blue, 
+        White
+    }
+}
